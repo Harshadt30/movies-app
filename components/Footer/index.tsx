4d123872eb08f8325cf639b8b482eafc473bb7a1
harshad_t <tharshad50@gmail.com>
2023-04-03 10:19:25 +0530

    basic header footer desktop added

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,18 @@
+import Container from "../Container"
+import styles from "./Footer.styles.module.sass"
+
 type Props = {}
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>
+  return (
+    <footer className="bg-black text-white">
+      <Container>
+        <div className={styles.footerWrapper}>
+          <p>&copy; MovieFlixx {new Date().getFullYear()} all right reserved</p>
+        </div>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,20 +1,51 @@
 import Image from "next/image"
 import Link from "next/link"
+import Container from "../Container"
+import styles from "./Header.styles.module.sass"
 type Props = {}
+
+const linkWrapperStyles = "inline-flex gap-5"
+const linkStyles = "px-3 py-6"
 
 const Header = (props: Props) => {
   return (
-    <header>
-      <div>
-        <Link href="/">
-          <Image src="/logo.png" alt="MovieFlixx" width={250} height={100} />
-        </Link>
-      </div>
-      <nav>
-        <Link href="/movies">Movies</Link>
-        <Link href="/tv">Tv</Link>
-        <Link href="/discover">Divcover</Link>
-      </nav>
+    <header className="bg-black text-white">
+      <Container>
+        <div className={`${styles.headerWrapper}`}>
+          <div>
+            <Link href="/" title="MovieFlixx">
+              <Image
+                src="/logo.png"
+                alt="MovieFlixx"
+                width={250}
+                height={100}
+              />
+            </Link>
+          </div>
+          <nav className={linkWrapperStyles}>
+            <Link className={linkStyles} href="/movies">
+              Movies
+            </Link>
+            <Link className={linkStyles} href="/tv">
+              Tv
+            </Link>
+            <Link className={linkStyles} href="/discover">
+              Divcover
+            </Link>
+            <Link className={linkStyles} href="/trending">
+              Trending
+            </Link>
+          </nav>
+          <div className={linkWrapperStyles}>
+            <Link className={linkStyles} href="/login">
+              Login
+            </Link>
+            <Link className={linkStyles} href="/register">
+              SignUp
+            </Link>
+          </div>
+        </div>
+      </Container>
     </header>
   )
 }
