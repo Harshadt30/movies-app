c85f9f5e50d4fa95e10592adba47c94951356398
Harshad Tahiliani <58172026+Harshadt30@users.noreply.github.com>
2023-04-03 10:21:04 +0530

    Layout setup (#6)
    * layout setup init
    
    * basic header footer desktop added

---
+++ components/Header/index.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image"
+import Link from "next/link"
+import Container from "../Container"
+import styles from "./Header.styles.module.sass"
+type Props = {}
+
+const linkWrapperStyles = "inline-flex gap-5"
+const linkStyles = "px-3 py-6"
+
+const Header = (props: Props) => {
+  return (
+    <header className="bg-black text-white">
+      <Container>
+        <div className={`${styles.headerWrapper}`}>
+          <div>
+            <Link href="/" title="MovieFlixx">
+              <Image
+                src="/logo.png"
+                alt="MovieFlixx"
+                width={250}
+                height={100}
+              />
+            </Link>
+          </div>
+          <nav className={linkWrapperStyles}>
+            <Link className={linkStyles} href="/movies">
+              Movies
+            </Link>
+            <Link className={linkStyles} href="/tv">
+              Tv
+            </Link>
+            <Link className={linkStyles} href="/discover">
+              Divcover
+            </Link>
+            <Link className={linkStyles} href="/trending">
+              Trending
+            </Link>
+          </nav>
+          <div className={linkWrapperStyles}>
+            <Link className={linkStyles} href="/login">
+              Login
+            </Link>
+            <Link className={linkStyles} href="/register">
+              SignUp
+            </Link>
+          </div>
+        </div>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
