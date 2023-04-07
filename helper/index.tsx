b32867b7f940704d55aa5c766792176f6e305493
Harshad Tahiliani <58172026+Harshadt30@users.noreply.github.com>
2023-04-07 18:43:12 +0530

    The movie db setup (#9)
    * added fetch hook for moviedb
    
    * updated type alias and comp update
    
    * settings change
    
    * moved code inside

--- helper/index.tsx
+++
@@ -1,6 +0,0 @@
-import { MOVIEDB_API_KEY, MOVIEDB_DOMAIN } from "../constant/MOVIEDB"
-
-export const generateMovieDBUrl = (params: string[]): string => {
-  const url = `${MOVIEDB_DOMAIN}${params.join("/")}?api_key=${MOVIEDB_API_KEY}`
-  return url
-}
