623251dc09bc8ea1f6f0a9a81d4251bc17422c9d
harshadt <tharshad50@gmail.com>
2023-04-07 18:41:56 +0530

    moved code inside

--- constant/MOVIEDB.js
+++
@@ -1,3 +0,0 @@
-export const MOVIEDB_DOMAIN = process.env.MOVIEDB_URL
-export const MOVIEDB_IMAGE_DOMAIN = process.env.MOVIEDB_IMAGE_DOMAIN
-export const MOVIEDB_API_KEY = process.env.MOVIEDB_API_KEY
